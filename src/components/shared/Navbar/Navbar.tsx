ae1c7d90ef0cd7b971f05896d77039f3d79db4fd
mthtitumir <titumirgccc@gmail.com>
2024-05-27 08:48:06 +0600

    active navlink added

--- src/components/shared/Navbar/Navbar.tsx
+++ src/components/shared/Navbar/Navbar.tsx
@@ -1,25 +1,42 @@
 "use client";
 
-import assets from "@/assets";
 import useUserInfo from "@/hooks/useUserInfo";
 import { Icons } from "@/icons";
 import { logoutUser } from "@/services/serverActions/logoutUser";
-import { Box, Button, Container, Icon, Stack, Typography } from "@mui/material";
-import Image from "next/image";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 const Navbar = () => {
   const router = useRouter();
+  const pathname = usePathname();
+  console.log(pathname);
+  
   const user = useUserInfo();
 
   const handleLogOut = () => {
     logoutUser(router);
   };
 
+  const getLinkStyles = (href: string) => ({
+    textDecoration: "none",
+    color: pathname === href ? "#A8174E" : "inherit",
+    borderBottom: pathname === href ? "2px solid #A8174E" : "none",
+    transition: "all 0.3s ease-in-out",
+    "&:hover": {
+      color: "#A8174E",
+      borderBottom: "2px solid #A8174E",
+    },
+  });
+
   return (
     <Container>
-      <Stack py={2} direction="row" alignItems="center" justifyContent="space-between">
+      <Stack
+        py={2}
+        direction="row"
+        alignItems="center"
+        justifyContent="space-between"
+      >
         <Stack direction="row" gap={1} alignItems="center">
           <Box>
             <Icons.DropDouble size={20} color="#A8174E" />
@@ -33,10 +50,28 @@ const Navbar = () => {
           </Typography>
         </Stack>
         <Stack direction="row" alignItems="center" gap={5}>
-          <Typography>Home</Typography>
-          <Typography>About Us</Typography>
-          <Typography>Profile</Typography> {/*if logged in*/}
-          <Typography component={Link} href="/donors">
+          <Typography component={Link} href="/" sx={getLinkStyles("/")}>
+            Home
+          </Typography>
+          <Typography
+            component={Link}
+            href="/about-us"
+            sx={getLinkStyles("/about-us")}
+          >
+            About Us
+          </Typography>
+          <Typography
+            component={Link}
+            href="/dashboard/profile"
+            sx={getLinkStyles("/dashboard/profile")}
+          >
+            Profile
+          </Typography>
+          <Typography
+            component={Link}
+            href="/donors"
+            sx={getLinkStyles("/donors")}
+          >
             Find Blood
           </Typography>
           {user?.id ? (
